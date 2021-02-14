f8c8a68b4a0b512d1baafd911ce8364b0d34a8a3
Nolan Groenewold <nolanclark99@gmail.com>
2021-02-13 22:14:14 -0600

    Vue3 w/Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,19 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
+  <div id="nav" class="bg-fixed">
+    <router-link to="/">home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+export default {
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  setup(){
+    
   }
 }
-</style>
+</script>
+
+
